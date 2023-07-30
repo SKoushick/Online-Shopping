c350acf28d3a989b6543b52bc5c7926e9ab67ce1
SKoushick <koushick715@gmail.com>
2023-07-30 11:42:24 +0530

    Update PG2.py

--- PG2.py
+++ PG2.py
@@ -18,33 +18,7 @@ top.geometry("1200x1200")
 label = Label(top, image=bg)
 label.place(x = 0,y = 0)
 
-##def pur():  
-##                    messagebox.askquestion("Confirm","Are you sure to purchase?")
-##
-##def cart():
-##    global cursor
-##    s=cursor.execute('SELECT * FROM products')
-##    name = Label(top, text="---IMAGECON ONLINE SHOPPING---", bg="black", font=("Brush Script MT", 30, 'bold'),
-##                 fg="white")
-##    name.pack()
-##    title = Label(top, text='YOUR CART', bg="gray", font=("Arial black", 15, 'bold'),
-##                  fg="black").place(x=500, y=75)
-##    fra1 = Frame(top, bg='white', height=450, width=800)
-##    fra1.place(x=220, y=130)
-##    items = Label
-##    pro = ttk.Treeview(fra1, columns=(1, 2), show='headings', height='8')
-##    pro.pack()
-##    pro.heading(1, text='PRODUCT')
-##    pro.heading(2, text='PRICE')
-##
-##    a=0
-##    for x in s:
-##        tree.insert('', a, text='',values=x)
-##        a=a+1
-##    purchase=Button(fra1,text='purchase now ',bg='brown',fg='white',command=pur)
-##    purchase.pack
-##    total=Label(fra1,text=x)
-##    total.pack()
+
         
 
 
